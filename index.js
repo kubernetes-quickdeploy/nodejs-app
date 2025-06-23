aec7eebd2401f52cbd5e16268840d4cb0093ed87
kubernetes-quickdeploy <99710234+kubernetes-quickdeploy@users.noreply.github.com>
2025-06-23 17:23:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,12 @@
 const express = require('express');
 const app = express();
-const request = require('request');
-const wikip = require('wiki-infobox-parser');
 
-//ejs
-app.set("view engine", 'ejs');
+const PORT = process.env.PORT || 3000;
 
-//routes
-app.get('/', (req,res) =>{
-    res.render('index');
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js app running inside Docker!');
 });
 
-app.get('/index', (req,response) =>{
-    let url = "https://en.wikipedia.org/w/api.php"
-    let params = {
-        action: "opensearch",
-        search: req.query.person,
-        limit: "1",
-        namespace: "0",
-        format: "json"
-    }
-
-    url = url + "?"
-    Object.keys(params).forEach( (key) => {
-        url += '&' + key + '=' + params[key]; 
-    });
-
-    //get wikip search string
-    request(url,(err,res, body) =>{
-        if(err) {
-            response.redirect('404');
-        }
-            result = JSON.parse(body);
-            x = result[3][0];
-            x = x.substring(30, x.length); 
-            //get wikip json
-            wikip(x , (err, final) => {
-                if (err){
-                    response.redirect('404');
-                }
-                else{
-                    const answers = final;
-                    response.send(answers);
-                }
-            });
-    });
-
-    
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-
-//port
-app.listen(3000, console.log("Listening at port 3000..."))
